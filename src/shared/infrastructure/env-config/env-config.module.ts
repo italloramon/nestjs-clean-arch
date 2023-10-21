a49c2c377297dfbe017d7a373fb5d50731b788a6
italloramon <irvp@ic.ufal.br>
2023-10-20 21:25:52 -0300

    test: EnvConfigService unit tests

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,6 +1,6 @@
-import {DynamicModule, Module} from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
-import {ConfigModule, ConfigModuleOptions} from "@nestjs/config";
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { join } from 'node:path';
 
 @Module({
