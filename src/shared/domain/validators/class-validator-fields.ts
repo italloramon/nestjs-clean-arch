e147c204abf9e0dda31af8f9cb60bf76d215a5dc
italloramon <irvp@ic.ufal.br>
2023-10-22 20:46:31 -0300

    chore: add abstract class ClassValidatorFields

---
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -0,0 +1,26 @@
+import {
+  FieldsErrors,
+  ValidatorFields,
+} from '@/shared/domain/validators/validator-fields.interface';
+import { validateSync } from 'class-validator';
+
+export abstract class ClassValidatorFields<PropsValidated>
+  implements ValidatorFields<PropsValidated>
+{
+  errors: FieldsErrors = null;
+  validatedData: PropsValidated = null;
+
+  validate(data: any): boolean {
+    const errors = validateSync(data);
+    if (errors.length) {
+      this.errors = {};
+      for (const error of errors) {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      }
+    } else {
+      this.validatedData = data;
+    }
+    return !errors.length;
+  }
+}
