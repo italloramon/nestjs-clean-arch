a15abeacf8c2481fe4541cfc30f2eeb8f703967b
italloramon <irvp@ic.ufal.br>
2023-10-22 20:36:51 -0300

    chore: add ValidatorFields interface

---
+++ src/shared/domain/validators/validator-fields.interface.ts
@@ -0,0 +1,9 @@
+export type FieldsErrors = {
+  [field: string]: string[];
+};
+
+export interface ValidatorFields<PropsValidated> {
+  errors: FieldsErrors;
+  validatedData: PropsValidated;
+  validate(data: any): boolean;
+}
